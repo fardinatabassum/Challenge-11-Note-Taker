c2599986ab8702e92e00d184d82563a8391f7d54
fardinatabassum <fardinatabassum@gmail.com>
2023-03-13 17:32:49 -0400

    added js commenting and updated readme aong wirh demo gif

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Challenge-11-Note-Taker
-This repository contains the code used to build a note taker webpage
+
+## Description
+ This repository contains the code used to build a note-taker webpage. The purpose if this webpage is to allow users to be able to write and save their personal notes. The webpage also allows users to be able to delete their notes when they no longer need it. This webpage was built to compile all the users notes in an organized manner. The notes gets saved in the server as using the backend aspect of the webpage. The webpage also allows users to delete the notes from the webpage using the delete button
+## Installation
+
+Use the URL:
+[Deployed Website](https://desolate-dawn-46872.herokuapp.com/)
+
+## Usage
+
+![Challenge-11-Note-Taker](./Assets/Note%20taker%20demo%20.gif)
+
+The assets file contains the screenshot and demo gif of the final webpage webpage. The server.js file contains the code the provides the fucntionality of creating the deleting the notes. The public files contians the html, css and javascript files used to make the baisc webpage. The websote can be accessed using the link above. After opening the webpage click the get started button. Then to create a new notes press the plus icon. After filling in the title and description of the note save it usung the save icon on the top right corner. The saved notes are then displayed on the left column. By clicking the saved notes you can view the further description about that notes. The saved notes can then be deleted using the deleted icon located right next to the note.
+
+## License
+
+Uses the MIT License
+# [![License MIT ](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/M
--- server.js
+++ server.js
@@ -1,34 +1,42 @@
-const express = require('express');
-const path = require('path');
+const express = require('express'); // Import express js
+const path = require('path'); // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const fs = require('fs')
-const db = require("./db/db.json")
+const db = require("./db/db.json") 
 
-const PORT = process.env.PORT || 3001;
-const app = express()
+const PORT = process.env.PORT || 3001; // Specify on which port the Express.js server will run
+const app = express() // Initialize an instance of Express.js
 
+// Sets up the Express app to handle data parsing
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
-app.use(express.static('public'))
+app.use(express.static('public')) // Static middleware pointing to the public folder
 
 // HTML ROUTE
+// Create Express.js routes for default '/', '/index' and '/notes' endpoints
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+// GET request for notes
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
 
 // API ROUTES
+//Sends a JSON response with the correct content type.
+// GET request
 app.get('/api/notes', (req, res) => {
     res.json(db)
 })
 
+// POST request
 app.post('/api/notes', (req, res) => {
     res.json(createNote(req.body, db))
 })
 
+// creates new note and adds it to the empty array
 const createNote = (body, noteArr) => {
     body.id = noteArr.length;
     noteArr.push(body)
@@ -39,11 +47,13 @@ const createNote = (body, noteArr) => {
     return body;
 }
 
+// DELETE request
 app.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, db)
     res.json(true)
 })
 
+// deletes the node by matching the id of the note
 const deleteNote = (id, noteArr) => {
     for(let i = 0; i < noteArr.length; i++) {
         if(noteArr[i].id == id) {
@@ -56,6 +66,7 @@ const deleteNote = (id, noteArr) => {
     }
 }
 
+// listen() method is responsible for listening for incoming connections on the specified port 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
